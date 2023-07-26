7cfcdd5af942a536926bfa0dfcc571b855abc7e1
Anna <annybogatova@gmail.com>
2023-07-26 19:55:08 +0300

    add example file

--- result.asm
+++ result.asm
@@ -1,22 +1,26 @@
 INCLUDE Irvine32.inc
 .data
-aa SDWORD ?
-a SDWORD ?
-b SDWORD ?
+Xval SDWORD ?
+Yval SDWORD ?
+Rval SDWORD ?
+Zval SDWORD ?
 .code
 main PROC
-mov a, 6
-mov eax, a
-mov ebx, 3
+mov Xval, 26
+mov eax, Xval
+mov ebx, 4
 add eax, ebx
-mov aa, eax
-mov eax, aa
+mov Yval, eax
+mov Zval, 40
+mov eax, Xval
+mov ebx, Yval
+mov ecx, Zval
+sub ebx, ecx
+add eax, ebx
 mov ebx, 2
 imul eax, ebx
-mov b, eax
-mov eax, b
-mov ebx, 2
-add eax, ebx
+mov Rval, eax
+mov eax, Rval
 mov eax, eax
 call WriteDec
 exit
